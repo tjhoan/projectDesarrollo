d0f10ac746ec700b0cfadd76163d30607b44d9c6
tjhoan <tjhoanjose@gmail.com>
2024-11-26 00:27:27 -0500

    :bug::whale: Actualizar init.sh para cambiar las variables del ambiente test

--- init.sh
+++ init.sh
@@ -34,8 +34,8 @@ case $ENV in
     COMPOSE_FILES="-f docker-compose.yml -f docker-compose.test.yml"
     APP_CONTAINER="laravel-test"
     DB_CONTAINER="mysql-test"
-    DB_USER="laravel_user"
-    DB_PASS="laravel_pass"
+    DB_USER="test_user"
+    DB_PASS="test_pass"
     ;;
   *)
     echo "Entorno no válido. Usa: dev, prod o test"
